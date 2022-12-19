e7351143a01de20916798753b301f7fbbdc94da3
Andres Muñoz <andres.max36@gmail.com>
2022-12-19 11:46:51 +0100

    Update test_helper.js

--- autocorector/utils/test_helper.js
+++ autocorector/utils/test_helper.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 //tell mongoose to use es6 implementation of promises
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/bio_test');
+mongoose.connect('mongodb://127.0.0.1:27017/bio_test');
 
 
 // Close connection
